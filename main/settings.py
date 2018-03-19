d46449bbb551e229addc350abdcced45960642b9
Levi Zitting <levizitting5663@gmail.com>
2018-03-19 16:46:05 -0500

    webpack-loader setup

--- main/settings.py
+++ main/settings.py
@@ -128,21 +128,6 @@ STATICFILES_DIRS = [
 
 STATIC_URL = '/static/'
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ),
-}
-
-JWT_AUTH = {
-    'JWT_AUTH_HEADER_PREFIX': 'Bearer'
-}
-
 WEBPACK_LOADER = {
     'DEFAULT': {
         'CACHE': not DEBUG,
